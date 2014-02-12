5dcd2e5ca2a9ecf970dfb3679a29a52f1762be45
Christophe Jolif <cjolif@gmail.com>
2014-02-12 22:56:41 +0100

    fixes for intern

--- tests/sauce.js
+++ tests/sauce.js
@@ -4,65 +4,61 @@
 // Learn more about configuring this file at <https://github.com/theintern/intern/wiki/Configuring-Intern>.
 // These default settings work OK for most people. The options that *must* be changed below are the
 // packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
-define({
-	loader: {
-		// location of all the packages, relative to client.html
-		baseUrl: "../../.."
-	},
+define(["./intern"], function (intern) {
+	var config = {
+		// The port on which the instrumenting proxy will listen
+		proxyPort: 9000,
 
-	useLoader: {
-		"host-node": "requirejs",
-		"host-browser": "../../../requirejs/require.js"
-	},
+		// A fully qualified URL to the Intern proxy
+		proxyUrl: "http://localhost:9000/",
 
-	// The port on which the instrumenting proxy will listen
-	proxyPort: 9000,
+		// Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
+		// specified browser environments in the `environments` array below as well. See
+		// https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
+		// https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
+		// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
+		// automatically
+		capabilities: {
+			"selenium-version": "2.37.0",
+			"idle-timeout": 60
+		},
 
-	// A fully qualified URL to the Intern proxy
-	proxyUrl: "http://localhost:9000/",
+		// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
+		// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
+		// capabilities options specified for an environment will be copied as-is
+		environments: [
 
-	// Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
-	// specified browser environments in the `environments` array below as well. See
-	// https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
-	// https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
-	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
-	// automatically
-	capabilities: {
-		"selenium-version": "2.37.0",
-		"idle-timeout": 60
-	},
+			{ browserName: "internet explorer", version: "11", platform: "Windows 8.1" },
+			{ browserName: "internet explorer", version: "10", platform: "Windows 8" },
+			{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
+			{ browserName: "firefox", version: "25", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
+			{ browserName: "chrome", version: "", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
+		//	{ browserName: "android", platform: "Android" },
+			{ browserName: "iphone", platform: "OS X 10.8",	version: "6.1",
+				"device-orientation": "portrait", "selenium-version": "" }
+		],
 
-	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
-	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
-	// capabilities options specified for an environment will be copied as-is
-	environments: [
+		// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
+		maxConcurrency: 3,
 
-		{ browserName: "internet explorer", version: "11", platform: "Windows 8.1" },
-		{ browserName: "internet explorer", version: "10", platform: "Windows 8" },
-		{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
-		{ browserName: "firefox", version: "25", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
-		{ browserName: "chrome", version: "", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
-	//	{ browserName: "android", platform: "Android" },
-		{ browserName: "iphone", platform: "OS X 10.8",	version: "6.1",
-			"device-orientation": "portrait", "selenium-version": "" }
-	],
+		// Whether or not to start Sauce Connect before running tests
+		useSauceConnect: true,
 
-	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-	maxConcurrency: 3,
+		// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
+		// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
+		// publishing this configuration file somewhere
+		webdriver: {
+			host: "localhost",
+			port: 4444
+		},
 
-	// Whether or not to start Sauce Connect before running tests
-	useSauceConnect: true,
+		// A regular expression matching URLs to files that should not be included in code coverage analysis
+		excludeInstrumentation: /^(requirejs|dcl|dojo|platform|\/tests|.*\/themes)/
+	};
 
-	// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
-	// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
-	// publishing this configuration file somewhere
-	webdriver: {
-		host: "localhost",
-		port: 4444
-	},
+	for (var key in intern) {
+		config[key] = intern[key];
+	}
 
-	suites: [ "dcolor/tests/all" ],
-
-	// A regular expression matching URLs to files that should not be included in code coverage analysis
-	excludeInstrumentation: /^(requirejs|dcl|dojo|platform|\/tests|.*\/themes)/
+	return config;
 });
