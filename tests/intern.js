b7d5b44e793e32cd1911ba79b90e6af5138021a3
Christophe Jolif <cjolif@gmail.com>
2014-02-13 16:42:19 +0100

    requirejs is needed only on browser + improve mobile env

--- tests/intern.js
+++ tests/intern.js
@@ -1,14 +1,8 @@
 // Test file to run infrastructure tests from a browser
-// Run using http://localhost/delite/node_modules/intern/client.html?config=tests/client
+// Run using http://localhost/delite/node_modules/intern/client.html?config=tests/intern
 
 define({
-	loader: {
-		// location of all the packages, relative to client.html
-		baseUrl: "../../.."
-	},
-
 	useLoader: {
-		"host-node": "requirejs",
 		"host-browser": "../../../requirejs/require.js"
 	},
 
--- tests/sauce.js
+++ tests/sauce.js
@@ -27,15 +27,19 @@ define(["./intern"], function (intern) {
 		// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 		// capabilities options specified for an environment will be copied as-is
 		environments: [
-
 			{ browserName: "internet explorer", version: "11", platform: "Windows 8.1" },
 			{ browserName: "internet explorer", version: "10", platform: "Windows 8" },
 			{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
 			{ browserName: "firefox", version: "25", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
 			{ browserName: "chrome", version: "", platform: [ /*"OS X 10.6", "Linux", */ "Windows 7" ] },
-		//	{ browserName: "android", platform: "Android" },
-			{ browserName: "iphone", platform: "OS X 10.8",	version: "6.1",
+			{ browserName: "iphone", platform: "OS X 10.8", version: "6.1",
 				"device-orientation": "portrait", "selenium-version": "" }
+			/*
+			{ browserName: "android", platform: "Linux", version: "4.1" },
+			{ browserName: "android", platform: "Linux", version: "4.0" },
+			{ browserName: "iphone", platform: "OS X 10.9", version: "7" },
+			{ browserName: "iphone", platform: "OS X 10.8", version: "6.1" },
+			{ browserName: "iphone", platform: "OS X 10.8", version: "6.0" }*/
 		],
 
 		// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
